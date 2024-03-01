3ca8c9a9afeccec88550702cbfdfd82443df4eb9
athletefactory <160275745+athletefactory@users.noreply.github.com>
2024-03-01 11:10:24 -0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -349,9 +349,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "69644b52b2af67c6444c06a6fbe9db8e
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7890b178c6c4ac0c26642422fe499a63",
-"/": "7890b178c6c4ac0c26642422fe499a63",
-"main.dart.js": "8e5879749f5edcafa7243d2129ad3ad9",
+"index.html": "2e5bc0d4b0485df958d4b942ebd80889",
+"/": "2e5bc0d4b0485df958d4b942ebd80889",
+"main.dart.js": "453c66423e5204481fd517438f6164b7",
 "manifest.json": "93fc3d688c07415deac81ba441ce929e",
 "version.json": "ce63b45088a131a0893f94e032a6ff3d"};
 // The application shell files that are downloaded before a service worker can
