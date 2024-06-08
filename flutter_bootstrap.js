75ef38b56a119b8249176babab8f69d780622b5f
athletefactory <160275745+athletefactory@users.noreply.github.com>
2024-06-08 23:29:23 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1271212588"
+    serviceWorkerVersion: "2150239469"
   }
 });
