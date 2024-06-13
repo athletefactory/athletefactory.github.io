8d46a7aadc30787b62c3a4842cb16fd67effc8bd
athletefactory <160275745+athletefactory@users.noreply.github.com>
2024-06-13 20:14:12 +0300

    Delete lib directory

--- lib/functions/platform-type.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'dimensions.dart';
-
-bool isDesktop(BuildContext context) => screenWidth(context) >= 800;
-
-bool isMobile(BuildContext context) => screenWidth(context) < 800;
